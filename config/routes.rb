d085ca583e55e9e1379a9e01ab51c40316cc7d2f
Ubuntu <ubuntu@ip-172-31-18-192.ap-northeast-1.compute.internal>
2025-09-25 05:04:52 +0000

    [feat] setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'user/show'
+  get 'user/edit'
+  get 'user/index'
+  get 'books/new'
+  get 'books/index'
+  get 'books/edit'
+  get 'books/show'
+  get 'homes/top'
+  get 'homes/about'
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
