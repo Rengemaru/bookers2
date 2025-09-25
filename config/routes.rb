328602e09a052d7f68ca07de87da465c8a3e4ae9
Ubuntu <ubuntu@ip-172-31-18-192.ap-northeast-1.compute.internal>
2025-09-25 05:09:42 +0000

    [feat] ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,18 @@
 Rails.application.routes.draw do
-  get 'user/show'
-  get 'user/edit'
-  get 'user/index'
-  get 'books/new'
-  get 'books/index'
-  get 'books/edit'
-  get 'books/show'
-  get 'homes/top'
-  get 'homes/about'
+  # get 'user/show'
+  # get 'user/edit'
+  # get 'user/index'
+  # get 'books/new'
+  # get 'books/index'
+  # get 'books/edit'
+  # get 'books/show'
+  # get 'homes/top'
+  # get 'homes/about'
   devise_for :users
+  root to: 'homes#top'
+
+  resources :books, only: [:new, :create, :index, :show, :edit, :destroy]
+  resources :users, only: [:index, :show, :update]
+  get 'homes/about' => 'homes#about', as: 'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
