598b9ed08bad0366e48cc417004db86a5f555573
Ubuntu <ubuntu@ip-172-31-18-192.ap-northeast-1.compute.internal>
2025-09-25 06:56:37 +0000

    [] [feat] homes

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,6 +2,11 @@ class BooksController < ApplicationController
   def new
   end
 
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to books_path
   def index
   end
 
@@ -10,4 +15,9 @@ class BooksController < ApplicationController
 
   def show
   end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :opinion)
 end
